476f028d74c933b256b078902b02ccef1956bf72
Martin Cserép <martincserep1@gmail.com>
2020-02-11 21:26:18 +0100

    improved calculator

--- calc.ps1
+++ calc.ps1
@@ -1,15 +1,33 @@
-if($args.count -eq 3) {
-    if($args[0] -eq 'add'){
-        printf($args[1] + '+' + $args[2] + '=' + ([int]$args[1] + [int]$args[2]))
-    } elseif($args[0] -eq 'sub'){
-        printf($args[1] + '-' + $args[2] + '=' + $args[1] - $args[2])
-    } elseif($args[0] -eq 'mult'){
-        printf($args[1] + '*' + $args[2] + '=' + $args[1] * $args[2])
-    } elseif($args[0] -eq 'divide'){
-        printf($args[1] + '/' + $args[2] + '=' + $args[1] / $args[2])
-    } else {
-        printf("wrong")
+do {
+    $operation = Read-Host -Prompt 'What would like to do? "add" "sub" "mult" "divide"'
+} until (($operation -eq 'add') -or ($operation -eq 'sub') -or ($operation -eq 'mult') -or ($operation -eq 'divide'))
+
+do {
+    try {
+        $numOk = $true
+        [int]$a = Read-host "First number"
+        }
+    catch {$numOK = $false}
     }
+until ($numOK)
+
+do {
+    try {
+        $numOk = $true
+        [int]$b = Read-Host 'Second number"'
+        }
+    catch {$numOK = $false}
+    }
+until ($numOK)
+
+if($operation -eq 'add'){
+    printf($a.ToString() + '+' + $b.ToString() + '=' + ($a + $b))
+} elseif($operation -eq 'sub'){
+    printf($a.ToString() + '-' + $b.ToString() + '=' + ($a - $b))
+} elseif($operation -eq 'mult'){
+    printf($a.ToString() + '*' + $b.ToString() + '=' + ($a * $b))
+} elseif($operation -eq 'divide'){
+    printf($a.ToString() + '/' + $b.ToString() + '=' + ($a / $b))
 } else {
-    printf("Wrong arguments")
+    printf("wrong")
 }
